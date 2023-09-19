c54baed089cdce5dca33dd40073fd902ef349cc4
Bill Wake <william.wake@acm.org>
2023-09-19 09:41:12 -0500

    initial xcode project

---
+++ dulce/dulceApp.swift
@@ -0,0 +1,17 @@
+//
+//  dulceApp.swift
+//  dulce
+//
+//  Created by Bill Wake on 9/19/23.
+//
+
+import SwiftUI
+
+@main
+struct dulceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
